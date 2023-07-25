3ea331887eec6bbf57c48d02e8921dbec31ba74c
Jeong Wonjun <67366101+Pongchi@users.noreply.github.com>
2023-07-25 13:23:31 +0000

    init

---
+++ glimelight/src/main/java/com/pongchi/glimelight/GlimelightApplication.java
@@ -0,0 +1,13 @@
+package com.pongchi.glimelight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlimelightApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GlimelightApplication.class, args);
+	}
+
+}
