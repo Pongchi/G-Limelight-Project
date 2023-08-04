36f3057fb9ea8c870b27b86dc0d04ec2e4ffd47c
Jeong Wonjun <67366101+Pongchi@users.noreply.github.com>
2023-08-04 14:02:58 +0000

    add 2 Test Code in SubscribeServiceTest

--- src/test/java/com/pongchi/glimelight/service/SubscribeServiceTest.java
+++ src/test/java/com/pongchi/glimelight/service/SubscribeServiceTest.java
@@ -1,11 +1,17 @@
 package com.pongchi.glimelight.service;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.pongchi.glimelight.domain.member.Member;
 import com.pongchi.glimelight.domain.member.MemberRepository;
+import com.pongchi.glimelight.domain.subscribe.Subscribe;
 import com.pongchi.glimelight.domain.subscribe.SubscribeRepository;
 
 @Transactional
@@ -22,7 +28,49 @@ public class SubscribeServiceTest {
     private SubscribeRepository subscribeRepository;
 
     @Test
+    public void 구독_추가() {
+        // given
+        String fromEmail = "fromtest@email.com";
+        String fromPassword = "fromtest";
+        String fromNickname = "fromMember";
+        Member fromMember = new Member(fromEmail, fromPassword, fromNickname);
+        memberRepository.save(fromMember);
+
+        String toEmail = "totest@email.com";
+        String toPassword = "totest";
+        String toNickname = "toMember";
+        Member toMember = new Member(toEmail, toPassword, toNickname);
+        memberRepository.save(toMember);
+
+        // when
+        long addedSubsribeId = subscribeService.addSubscribe(fromMember.getId(), toMember.getId());
+
+        // then
+        Subscribe subscribe = subscribeRepository.findById(addedSubsribeId).get();
+        assertEquals(subscribe.getId(), addedSubsribeId);
+        assertEquals(subscribe.getFromMember().getId().toString(), fromMember.getId().toString());
+        assertEquals(subscribe.getToMember().getId().toString(), toMember.getId().toString());
+    }
+
+    @Test
     public void 멤버를_향한_구독_모두_찾기() {
-        
+        // given
+        String fromEmail = "fromtest@email.com";
+        String fromPassword = "fromtest";
+        String fromNickname = "fromMember";
+        Member fromMember = new Member(fromEmail, fromPassword, fromNickname);
+        memberRepository.save(fromMember);
+
+        String toEmail = "totest@email.com";
+        String toPassword = "totest";
+        String toNickname = "toMember";
+        Member toMember = new Member(toEmail, toPassword, toNickname);
+        memberRepository.save(toMember);
+
+        long addedSubscribeId = subscribeService.addSubscribe(fromMember.getId(), toMember.getId());
+
+        // when
+        Page<Subscribe> subscribes = subscribeService.findAllByToMember(toMember.getId(), Pageable.ofSize(1));
+        assertEquals(subscribes.getContent().get(0).getId(), addedSubscribeId);
     }
 }
