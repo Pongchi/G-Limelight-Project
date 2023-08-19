a29397fb192720f93ca83b3473bf14ff63104aa0
Jeong Wonjun <67366101+Pongchi@users.noreply.github.com>
2023-08-19 07:46:43 +0000

    chane SecurityConfig

--- src/main/java/com/pongchi/glimelight/config/SecurityConfig.java
+++ src/main/java/com/pongchi/glimelight/config/SecurityConfig.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -16,6 +16,20 @@ import lombok.RequiredArgsConstructor;
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
+    
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        // 정적 자원에 대해서 Security를 적용하지 않음으로 설정
+        return web -> web.ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations());
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+            .authorizeHttpRequests(authz -> authz.anyRequest().permitAll());
+
+        return http.build();
+    }
 
     @Bean
     PasswordEncoder passwordEncoder() {
