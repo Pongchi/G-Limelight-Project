f35ce5f95e1efdab1247ed90b46c4a44fd94a441
Jeong Wonjun <67366101+Pongchi@users.noreply.github.com>
2023-08-22 13:46:52 +0000

    add Validation in SubscribeController

--- src/main/java/com/pongchi/glimelight/api/v1/SubscribeController.java
+++ src/main/java/com/pongchi/glimelight/api/v1/SubscribeController.java
@@ -1,9 +1,13 @@
 package com.pongchi.glimelight.api.v1;
 
+import static com.pongchi.glimelight.api.v1.dto.ResponseDto.createResponseEntity;
+import static com.pongchi.glimelight.api.v1.dto.ResponsesDto.createResponsesEntity;
+
 import java.util.UUID;
 
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,12 +15,10 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import static com.pongchi.glimelight.api.v1.dto.ResponseDto.createResponseEntity;
-import static com.pongchi.glimelight.api.v1.dto.ResponsesDto.createResponsesEntity;
-
 import com.pongchi.glimelight.common.ResponseCode;
 import com.pongchi.glimelight.service.SubscribeService;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
@@ -26,7 +28,14 @@ public class SubscribeController {
     private final SubscribeService subscribeService;
     
     @GetMapping("/api/v1/subscribe")
-    public ResponseEntity<?> findAllByToMember(@RequestParam UUID toMemberId, Pageable pageable) {
+    public ResponseEntity<?> findAllByToMember(@NotBlank @RequestParam UUID toMemberId, Pageable pageable, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return createResponsesEntity(
+                ResponseCode.INVALID_PARAMETER, 
+                bindingResult.getAllErrors()
+            );
+        }
+        
         return createResponsesEntity(
             ResponseCode.SUCCESS,
             subscribeService.findAllByToMember(toMemberId, pageable).getContent()
@@ -34,7 +43,14 @@ public class SubscribeController {
     }
 
     @PostMapping("/api/v1/subscribe")
-    public ResponseEntity<?> addSubscribe(@RequestBody UUID from, @RequestBody UUID to) {
+    public ResponseEntity<?> addSubscribe(@NotBlank @RequestBody UUID from, @NotBlank @RequestBody UUID to, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return createResponsesEntity(
+                ResponseCode.INVALID_PARAMETER, 
+                bindingResult.getAllErrors()
+            );
+        }
+
         return createResponseEntity(
             ResponseCode.SUCCESS,
             subscribeService.addSubscribe(from ,to)
@@ -42,7 +58,14 @@ public class SubscribeController {
     }
 
     @DeleteMapping("/api/v1/subscribe")
-    public ResponseEntity<?> subSubscribe(@RequestBody UUID from, @RequestBody UUID to) {
+    public ResponseEntity<?> subSubscribe(@NotBlank @RequestBody UUID from, @NotBlank @RequestBody UUID to, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return createResponsesEntity(
+                ResponseCode.INVALID_PARAMETER, 
+                bindingResult.getAllErrors()
+            );
+        }
+
         return createResponseEntity(
             ResponseCode.SUCCESS,
             subscribeService.subSubscribe(from ,to)
@@ -50,7 +73,13 @@ public class SubscribeController {
     }
 
     @GetMapping("/api/v1/subscribe/check")
-    public ResponseEntity<?> check(@RequestParam UUID fromMemberId, @RequestParam UUID toMemberId) {
+    public ResponseEntity<?> check(@NotBlank @RequestParam UUID fromMemberId, @NotBlank @RequestParam UUID toMemberId, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return createResponsesEntity(
+                ResponseCode.INVALID_PARAMETER, 
+                bindingResult.getAllErrors()
+            );
+        }
         return createResponseEntity(
             ResponseCode.SUCCESS,
             subscribeService.check(fromMemberId, toMemberId)
