bd5b4ec15eef24f44334eae0a697992c91641476
Jeong Wonjun <67366101+Pongchi@users.noreply.github.com>
2023-08-22 13:50:14 +0000

    add Validation in LikeController

--- src/main/java/com/pongchi/glimelight/api/v1/LikeController.java
+++ src/main/java/com/pongchi/glimelight/api/v1/LikeController.java
@@ -3,6 +3,7 @@ package com.pongchi.glimelight.api.v1;
 import java.util.UUID;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,9 +11,11 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import static com.pongchi.glimelight.api.v1.dto.ResponseDto.createResponseEntity;
+import static com.pongchi.glimelight.api.v1.dto.ResponsesDto.createResponsesEntity;
 import com.pongchi.glimelight.common.ResponseCode;
 import com.pongchi.glimelight.service.LikeService;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
@@ -22,7 +25,14 @@ public class LikeController {
     private final LikeService likeService;
 
     @GetMapping("/api/v1/like")
-    public ResponseEntity<?> check(@RequestParam UUID postId, @RequestParam UUID memberId) {
+    public ResponseEntity<?> check(@NotBlank @RequestParam UUID postId, @NotBlank @RequestParam UUID memberId, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return createResponsesEntity(
+                ResponseCode.INVALID_PARAMETER, 
+                bindingResult.getAllErrors()
+            );
+        }
+
         return createResponseEntity(
             ResponseCode.SUCCESS,
             likeService.check(postId, memberId)
@@ -30,7 +40,14 @@ public class LikeController {
     }
     
     @PostMapping("/api/v1/like")
-    public ResponseEntity<?> addLike(@RequestParam UUID postId, @RequestParam UUID memberId) {
+    public ResponseEntity<?> addLike(@NotBlank @RequestParam UUID postId, @NotBlank @RequestParam UUID memberId, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return createResponsesEntity(
+                ResponseCode.INVALID_PARAMETER, 
+                bindingResult.getAllErrors()
+            );
+        }
+
         return createResponseEntity(
             ResponseCode.SUCCESS,
             likeService.add(postId, memberId)
@@ -38,7 +55,14 @@ public class LikeController {
     }
 
     @DeleteMapping("/api/v1/like")
-    public ResponseEntity<?> subLike(@RequestParam UUID postId, @RequestParam UUID memberId) {
+    public ResponseEntity<?> subLike(@NotBlank @RequestParam UUID postId, @NotBlank @RequestParam UUID memberId, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return createResponsesEntity(
+                ResponseCode.INVALID_PARAMETER, 
+                bindingResult.getAllErrors()
+            );
+        }
+        
         return createResponseEntity(
             ResponseCode.SUCCESS,
             likeService.subtract(postId, memberId)
