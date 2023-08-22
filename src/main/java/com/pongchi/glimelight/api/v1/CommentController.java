38e32df2004f465119c7c9a81e1dd1b40f06559b
Jeong Wonjun <67366101+Pongchi@users.noreply.github.com>
2023-08-22 13:18:58 +0000

    add all ResponseEntity in Controller

--- src/main/java/com/pongchi/glimelight/api/v1/CommentController.java
+++ src/main/java/com/pongchi/glimelight/api/v1/CommentController.java
@@ -1,17 +1,20 @@
 package com.pongchi.glimelight.api.v1;
 
-import java.util.List;
 import java.util.UUID;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import static com.pongchi.glimelight.api.v1.dto.ResponseDto.createResponseEntity;
+import static com.pongchi.glimelight.api.v1.dto.ResponsesDto.createResponsesEntity;
+
 import com.pongchi.glimelight.api.v1.dto.comment.CommentCreationDto;
 import com.pongchi.glimelight.api.v1.dto.comment.CommentOnPostRequestDto;
-import com.pongchi.glimelight.domain.comment.Comment;
+import com.pongchi.glimelight.common.ResponseCode;
 import com.pongchi.glimelight.service.CommentService;
 
 import lombok.RequiredArgsConstructor;
@@ -23,18 +26,26 @@ public class CommentController {
     private final CommentService commentService;
 
     @GetMapping("/api/v1/comments")
-    public List<Comment> getCommentsOnPost(CommentOnPostRequestDto requestDto) {
-        return commentService.getCommentsOnPost(requestDto);
+    public ResponseEntity<?> getCommentsOnPost(CommentOnPostRequestDto requestDto) {
+        return createResponsesEntity(
+            ResponseCode.SUCCESS,
+            commentService.getCommentsOnPost(requestDto)
+        );
     }
 
     @PostMapping("/api/v1/comments")
-    public Comment create(CommentCreationDto requestDto) {
-        return commentService.create(requestDto);
+    public ResponseEntity<?> create(CommentCreationDto requestDto) {
+        return createResponseEntity(
+            ResponseCode.SUCCESS,
+            commentService.create(requestDto)
+        );
     }
 
     @DeleteMapping("/api/v1/comments/{id}")
-    public Comment delete(@PathVariable UUID id) {
-        return commentService.delete(id);
+    public ResponseEntity<?> delete(@PathVariable UUID id) {
+        return createResponseEntity(
+            ResponseCode.SUCCESS,
+            commentService.delete(id)
+        );
     }
-
 }
--- src/main/java/com/pongchi/glimelight/api/v1/LikeController.java
+++ src/main/java/com/pongchi/glimelight/api/v1/LikeController.java
@@ -2,14 +2,15 @@ package com.pongchi.glimelight.api.v1;
 
 import java.util.UUID;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.pongchi.glimelight.api.v1.dto.like.LikeCheckDto;
-import com.pongchi.glimelight.domain.like.Like;
+import static com.pongchi.glimelight.api.v1.dto.ResponseDto.createResponseEntity;
+import com.pongchi.glimelight.common.ResponseCode;
 import com.pongchi.glimelight.service.LikeService;
 
 import lombok.RequiredArgsConstructor;
@@ -21,17 +22,26 @@ public class LikeController {
     private final LikeService likeService;
 
     @GetMapping("/api/v1/like")
-    public LikeCheckDto check(@RequestParam UUID postId, @RequestParam UUID memberId) {
-        return likeService.check(postId, memberId);
+    public ResponseEntity<?> check(@RequestParam UUID postId, @RequestParam UUID memberId) {
+        return createResponseEntity(
+            ResponseCode.SUCCESS,
+            likeService.check(postId, memberId)
+        );
     }
     
     @PostMapping("/api/v1/like")
-    public Like addLike(@RequestParam UUID postId, @RequestParam UUID memberId) {
-        return likeService.add(postId, memberId);
+    public ResponseEntity<?> addLike(@RequestParam UUID postId, @RequestParam UUID memberId) {
+        return createResponseEntity(
+            ResponseCode.SUCCESS,
+            likeService.add(postId, memberId)
+        );
     }
 
     @DeleteMapping("/api/v1/like")
-    public Like subLike(@RequestParam UUID postId, @RequestParam UUID memberId) {
-        return likeService.subtract(postId, memberId);
+    public ResponseEntity<?> subLike(@RequestParam UUID postId, @RequestParam UUID memberId) {
+        return createResponseEntity(
+            ResponseCode.SUCCESS,
+            likeService.subtract(postId, memberId)
+        );
     }
 }
