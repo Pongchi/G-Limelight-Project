dac1ea4b983eb4310a3df94ee2fb344b9c385111
Jeong Wonjun <67366101+Pongchi@users.noreply.github.com>
2023-08-28 09:54:32 +0000

    update ExceptionHandler & ResponseCode

--- src/main/java/com/pongchi/glimelight/common/ResponseCode.java
+++ src/main/java/com/pongchi/glimelight/common/ResponseCode.java
@@ -11,9 +11,14 @@ public enum ResponseCode {
 
     //400 BAD_REQUEST 잘못된 요청
     INVALID_PARAMETER(400, "파라미터 값을 확인해주세요."),
+    NOT_FOUNDED_PARAMETER(400, "필요한 파라미터 값을 확인해주세요."),
+
+    // 컨트롤러에 없는 경로
+    INVALID_PATH(404, "잘못된 경로입니다."),
 
     //500 INTERNAL SERVER ERROR
-    INTERNAL_SERVER_ERROR(500, "서버 에러입니다. 서버 팀에 연락주세요!");
+    INTERNAL_SERVER_ERROR(
+        500, "서버 에러입니다.");
     
     private final int status;
     private final String message;
--- src/main/java/com/pongchi/glimelight/exception/CustomExceptionHandler.java
+++ src/main/java/com/pongchi/glimelight/exception/CustomExceptionHandler.java
@@ -1,8 +1,10 @@
 package com.pongchi.glimelight.exception;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.NoHandlerFoundException;
 
 import com.pongchi.glimelight.api.v1.dto.ResponseDto;
 import com.pongchi.glimelight.common.ResponseCode;
@@ -10,7 +12,7 @@ import com.pongchi.glimelight.common.ResponseCode;
 @RestControllerAdvice
 public class CustomExceptionHandler {
     @ExceptionHandler({ CustomException.class })
-    protected ResponseEntity<?> handleCustomException(CustomException ex) {
+    private ResponseEntity<?> handleCustomException(CustomException ex) {
         return ResponseDto.createResponseEntity(
             ex.getResponseCode(),
             null
@@ -18,9 +20,25 @@ public class CustomExceptionHandler {
     }
 
     @ExceptionHandler({ Exception.class })
-    protected ResponseEntity<?> handleServerException(Exception ex) {
+    private ResponseEntity<?> handleServerException(Exception ex) {
         return ResponseDto.createResponseEntity(
             ResponseCode.INTERNAL_SERVER_ERROR,
+            null
+        );
+    }
+
+    @ExceptionHandler({ MissingServletRequestParameterException.class })
+    private ResponseEntity<?> notFoundedParameterException(Exception ex) {
+        return ResponseDto.createResponseEntity(
+            ResponseCode.NOT_FOUNDED_PARAMETER,
+            null
+        );
+    }
+
+    @ExceptionHandler({ NoHandlerFoundException.class })
+    private ResponseEntity<?> invalidPathException(Exception ex) {
+        return ResponseDto.createResponseEntity(
+            ResponseCode.NOT_FOUNDED_PARAMETER,
             null
         );
     }
