9b5c6c0ec6c601235f3f798f0163f1e4e6f74a38
Jeong Wonjun <67366101+Pongchi@users.noreply.github.com>
2023-08-29 13:51:53 +0000

    add security path

--- src/main/java/com/pongchi/glimelight/config/SecurityConfig.java
+++ src/main/java/com/pongchi/glimelight/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.pongchi.glimelight.config;
 
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -10,6 +11,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import com.pongchi.glimelight.jwt.JwtTokenProvider;
 
@@ -22,6 +24,12 @@ public class SecurityConfig {
 
     private final JwtTokenProvider jwtTokenProvider;
 
+    //권한 확인을 하지 않는 uri
+    private static final AntPathRequestMatcher[] PERMIT_ALL_PATTERNS = new AntPathRequestMatcher[] {
+        AntPathRequestMatcher.antMatcher("/api/v1/members"),
+        AntPathRequestMatcher.antMatcher("/api/v1/members/login"),
+    };
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         // Disable csrf to use token
@@ -32,6 +40,15 @@ public class SecurityConfig {
         http
             .sessionManagement()
             .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        
+        http
+            .authorizeHttpRequests(request -> 
+                request
+                    .requestMatchers(PERMIT_ALL_PATTERNS)
+                    .permitAll()
+                    .requestMatchers(AntPathRequestMatcher.antMatcher("/api/**"))
+                    .authenticated()
+        );
 
         // Apply JWT
         http.apply(new JwtSecurityConfig(jwtTokenProvider));
