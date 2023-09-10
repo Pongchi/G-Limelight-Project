7dbcbe3b9c8e14cbe98865a3ebcf7368d56f129d
Jeong Wonjun <67366101+Pongchi@users.noreply.github.com>
2023-09-10 05:23:51 +0000

    add getMemberInfo TestCode

--- src/test/java/com/pongchi/glimelight/api/v1/MemberControllerTest.java
+++ src/test/java/com/pongchi/glimelight/api/v1/MemberControllerTest.java
@@ -2,17 +2,27 @@ package com.pongchi.glimelight.api.v1;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.UUID;
+
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.pongchi.glimelight.api.v1.dto.member.MemberLoginRequestDto;
 import com.pongchi.glimelight.api.v1.dto.member.MemberRegisterRequestDto;
 import com.pongchi.glimelight.domain.member.MemberRepository;
 
@@ -49,5 +59,47 @@ class MemberControllerTest {
 
         //then
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+    }
+
+    @Test
+    void getMemberInfo() throws JsonProcessingException, ParseException{
+        //given
+        String email = "test@email.com";
+        String password = "test_password";
+        String nickname = "test_nickname";
+
+        // 회원가입
+        MemberRegisterRequestDto registerRequestDto = new MemberRegisterRequestDto(email, password, nickname);
+        String registerUrl = "http://localhost:"+port+"/api/v1/members";
+        restTemplate.postForEntity(registerUrl, registerRequestDto, Object.class);
+
+        // 로그인
+        MemberLoginRequestDto loginRequestDto = new MemberLoginRequestDto(email, password);
+        String loginUrl = "http://localhost:"+port+"/api/v1/login";
+        ResponseEntity<?> loginResponse = restTemplate.postForEntity(loginUrl, loginRequestDto, Object.class);
+
+        JSONParser parser = new JSONParser();
+        JSONObject jsonObject = (JSONObject) parser.parse(loginResponse.getBody().toString());
+
+        String accessToken = jsonObject.get("result").toString();
+
+        // 정보 받아오기
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Authorization", "Bearer " + accessToken);
+        HttpEntity<?> request = new HttpEntity<>(headers);
+
+        UUID memberId = memberRepository.findAll().get(0).getId();
+        String findUrl = "http://localhost:"+port+"/api/v1/members/" + memberId;
+
+        // when
+        ResponseEntity<?> findResponse = restTemplate.exchange(
+            findUrl,
+            HttpMethod.GET,
+            request,
+            Object.class
+        );
+        
+        //then
+        assertThat(findResponse.getStatusCode()).isEqualTo(HttpStatus.OK);
     }
 }
